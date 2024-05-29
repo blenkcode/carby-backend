0883f14dc77e0f250af0ecb4979610c41c03447e
Your name <Your email>
2024-05-29 10:55:05 +0200

    get badges

--- models/badges.js
+++ models/badges.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const badgeSchema = mongoose.Schema({
-    name:String,
-    logo : String,
-    xp : Number,
-    task :
-    { type: mongoose.Schema.Types.ObjectId, ref: 'tasks' },
-    });
+  title: String,
+  img: String,
+  xp: Number,
+  category: String,
+});
 
-const Badge = mongoose.model('badges', badgeSchema);
+const Badge = mongoose.model("badges", badgeSchema);
 
 module.exports = Badge;
--- routes/badges.js
+++ routes/badges.js
@@ -13,4 +13,38 @@ router.get("/", (req, res) => {
     });
   });
 });
+
+router.post("/addBadge", async (req, res) => {
+  const { title, img, xp, category } = req.body;
+
+  Badges.findOne({ title: title })
+    .then((existingBadge) => {
+      if (existingBadge) {
+        return res.json({ result: false, error: "Badge already exists" });
+      }
+
+      const newBadges = new Badges({
+        title: title,
+        img: img,
+        xp: xp,
+        category: category,
+      });
+
+      newBadges
+        .save()
+        .then((newDoc) => {
+          res.json({
+            result: true,
+            badge: newDoc,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({ result: false, error: err.message });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({ result: false, error: err.message });
+    });
+});
+
 module.exports = router;
