3528b64e039df716a35eccb1ff4259638f49be0b
Your name <Your email>
2024-05-30 14:32:07 +0200

    lol

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const fileUpload = require("express-fileupload");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var tasksRouter = require("./routes/tasks");
@@ -21,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(fileUpload());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-const cloudinaryURL = process.env.CLOUDINARY_URL;
+
 const connectionString = process.env.CONNECTION_STRING;
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))
   .catch((error) => console.error(error));
-module.exports = {
-  cloudinaryURL,
-};
